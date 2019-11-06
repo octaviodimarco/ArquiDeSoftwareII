11cb9e67c068014990ba15adfef6b418ff4b21cc
Salva Diaz <1604171@ucc.edu.ar>
2019-11-06 10:57:41 -0300

    listo el model

--- src/main/java/ar/edu/ucc/arqsoft/test/model/Transaccion.java
+++ src/main/java/ar/edu/ucc/arqsoft/test/model/Transaccion.java
@@ -4,11 +4,61 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "TRANSACCION")
-public class Transaccion {
-    @Column(name = "FECHA", nullable = false)
+public class Transaccion extends ObjetoGenerico {
+    //Transacción: fecha, monto, operación (Debito, Crédito), tarjeta
+
+    @Column (name = "FECHA", nullable = false)
     private Date fecha;
+
+    @Column (name = "MONTO", nullable = false)
+    private Double monto;
+
+    @Enumerated (EnumType.ORDINAL)
+    @Column (name = "OPERACION", nullable = false)
+    private Operacion operacion;
+    
+    @OneToMany (fetch = FetchType.LAZY, targetEntity = Tarjeta.class)
+    @Column (name = "TARJETA", nullable = false)
+    private Tarjeta tarjeta;
+
+
+    public Date getFecha(){
+        return fecha;
+    }
+
+    public void setFecha (Date fecha){
+        this.fecha = fecha;
+    }
+
+    public Double getMonto(){
+        return monto;
+    }
+
+    public void setMonto(Double monto){
+        this.monto = monto;
+    }
+
+    public Operacion getOperacion(){
+        return operacion;
+    }
+
+    public void setOperacion(Operacion operacion){
+        this.operacion = operacion;
+    }
+
+    public Tarjeta getTarjeta(){
+        return tarjeta;
+    }
+
+    public void setTarjeta(Tarjeta tarjeta){
+        this.tarjeta = tarjeta;
+    }
 }
