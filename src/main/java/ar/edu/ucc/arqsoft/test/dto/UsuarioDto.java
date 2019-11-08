270965013894a173c8e0553ccc37383eabeabd3e
salvadiaz <salvadiazpesci@gmail.com>
2019-11-08 18:00:31 -0300

    usuarioDto

--- src/main/java/ar/edu/ucc/arqsoft/test/dto/UsuarioDto.java
+++ src/main/java/ar/edu/ucc/arqsoft/test/dto/UsuarioDto.java
@@ -28,7 +28,15 @@ public class UsuarioDto{
         this.id = id;
     }
 
-    public String getNobre(){
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getNombre(){
         return nombre;
     }
 
