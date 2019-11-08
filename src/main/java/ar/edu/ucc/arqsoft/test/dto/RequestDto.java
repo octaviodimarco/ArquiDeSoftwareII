e034f56484fefaefd64c0e824988064364796e92
Octavio <1610251@ucc.edu.ar>
2019-11-08 18:44:01 -0300

    Avances TarjetaService

--- src/main/java/ar/edu/ucc/arqsoft/test/dto/RequestDto.java
+++ src/main/java/ar/edu/ucc/arqsoft/test/dto/RequestDto.java
@@ -1,7 +1,5 @@
 package ar.edu.ucc.arqsoft.test.dto;
 
-import ar.edu.ucc.arqsoft.test.model.Tarjeta;
-
 public class RequestDto{
     
     private TarjetaDto tarjeta;
--- src/main/java/ar/edu/ucc/arqsoft/test/service/TarjetaService.java
+++ src/main/java/ar/edu/ucc/arqsoft/test/service/TarjetaService.java
@@ -3,6 +3,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.ucc.arqsoft.test.dao.DaoGenerico;
+import ar.edu.ucc.arqsoft.test.dto.ResponseDto;
+import ar.edu.ucc.arqsoft.test.dto.TarjetaAltaDto;
 import ar.edu.ucc.arqsoft.test.model.Tarjeta;
 import ar.edu.ucc.arqsoft.test.model.Transaccion;
 import ar.edu.ucc.arqsoft.test.model.Usuario;
@@ -23,7 +25,20 @@ public class TarjetaService{
     public ResponseDto nuevaTarjeta(TarjetaAltaDto tarjetaDto){
         Tarjeta tarjeta = new Tarjeta();
 
-        tarjeta.
+        tarjeta.setSaldo(tarjetaDto.getSaldo());
+        tarjeta.setNumero(tarjetaDto.getNumero());
+
+        Usuario usuario = usuarioDao.load(tarjetaDto.getUsuarioID());
+
+        
+
+        tarjeta.setUsuario(usuario);
+
+        tarjetaDao.saveOrUpdate(tarjeta);
+
+        tarjetaDto.setId(tarjeta.getId());
+
+
 
     }
     
