9e3ff1ba02c7b99837609e413011ef24d771be56
Octavio <1610251@ucc.edu.ar>
2019-11-08 17:08:18 -0300

    TarjetaDaoImp terminado

---
+++ src/main/java/ar/edu/ucc/arqsoft/test/dao/TarjetaDaoImp.java
@@ -0,0 +1,9 @@
+package ar.edu.ucc.arqsoft.test.dao;
+
+import org.springframework.stereotype.Repository;
+
+import ar.edu.ucc.arqsoft.test.model.Tarjeta;
+
+@Repository
+public class TarjetaDaoImp extends DaoGenericoImp<Tarjeta, Long> {
+}
