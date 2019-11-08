41ce606d50afd10bf82cd3c388a3bee250b915ae
salvadiaz <salvadiazpesci@gmail.com>
2019-11-08 17:13:14 -0300

    transaccionDaoImp y UsuarioDaoImp

---
+++ src/main/java/ar/edu/ucc/arqsoft/test/dao/TransaccionDaoImp.java
@@ -0,0 +1,9 @@
+
+package ar.edu.ucc.arqsoft.test.dao;
+
+import ar.edu.ucc.arqsoft.test.model.Transaccion;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TransaccionDaoImp extends DaoGenericoImp<Transaccion, Long> {
+}
---
+++ src/main/java/ar/edu/ucc/arqsoft/test/dao/UsuarioDaoImp.java
@@ -0,0 +1,8 @@
+package ar.edu.ucc.arqsoft.test.dao;
+
+import ar.edu.ucc.arqsoft.test.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UsuarioDaoImp extends DaoGenericoImp<Usuario, Long> {
+}
